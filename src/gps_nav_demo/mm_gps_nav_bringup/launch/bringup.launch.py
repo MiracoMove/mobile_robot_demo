396b581cfe1b8f6c861e8c45b91ec70d41508686
superkoh <koh.718@gmail.com>
2023-08-08 17:53:58 +0800

    add robot localization for sim

--- src/gps_nav_demo/mm_gps_nav_bringup/launch/bringup.launch.py
+++ src/gps_nav_demo/mm_gps_nav_bringup/launch/bringup.launch.py
@@ -5,9 +5,13 @@ Desc: Launch gps nav demo
 """
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess,
+                            IncludeLaunchDescription, TimerAction)
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (FindExecutable, LaunchConfiguration,
+                                  PathJoinSubstitution)
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -15,7 +19,9 @@ def generate_launch_description():  # pylint: disable=missing-function-docstring
     return LaunchDescription([
         *declare_launch_arguments(),
         launch_gps(),
-        launch_imu()
+        launch_imu(),
+        *launch_robot_localization(),
+        set_datum()
     ])
 
 
@@ -51,3 +57,75 @@ def launch_imu():
             "use_sim_time": LaunchConfiguration("use_sim_time"),
         }.items()
     )
+
+
+def launch_robot_localization():
+    """Launch robot_localization"""
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+
+    sim_pkg = FindPackageShare("mm_gps_nav_simulation")
+    sim_ekf_config = PathJoinSubstitution([sim_pkg, "configs", "ekf.yaml"])
+
+    return [
+        Node(
+            package="robot_localization",
+            executable="ekf_node",
+            name="ekf_filter_node_odom",
+            output="screen",
+            parameters=[
+                    sim_ekf_config,
+                    {"use_sim_time": use_sim_time},
+            ],
+            remappings=[("odometry/filtered", "odometry/local")],
+            condition=IfCondition(use_sim_time)
+        ),
+        Node(
+            package="robot_localization",
+            executable="ekf_node",
+            name="ekf_filter_node_map",
+            output="screen",
+            parameters=[
+                    sim_ekf_config,
+                    {"use_sim_time": use_sim_time},
+            ],
+            remappings=[
+                ("odometry/filtered", "odometry/global"),
+            ],
+            condition=IfCondition(use_sim_time)
+        ),
+        Node(
+            package="robot_localization",
+            executable="navsat_transform_node",
+            name="navsat_transform",
+            output="screen",
+            parameters=[
+                    sim_ekf_config,
+                    {"use_sim_time": use_sim_time},
+            ],
+            remappings=[
+                ("imu", "mm_gps_nav_imu_controller/imu/fix"),
+                ("gps/fix", "mm_gps_nav_gps_controller/gps/fix"),
+                ("gps/filtered", "gps/filtered"),
+                ("odometry/gps", "odometry/gps"),
+                ("odometry/filtered", "odometry/global"),
+            ],
+            condition=IfCondition(use_sim_time)
+        )
+    ]
+
+
+def set_datum():
+    """Init robot position for localization"""
+    set_gps_cmd = ExecuteProcess(
+        cmd=[
+            [
+                FindExecutable(name="ros2"),
+                " service call ",
+                "/datum ",
+                "robot_localization/srv/SetDatum ",
+                '"{geo_pose: {position: {latitude: 31.3943545, longitude: 121.2294018, altitude: 18}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}"',
+            ]
+        ],
+        shell=True,
+    )
+    return TimerAction(period=3.0, actions=[set_gps_cmd])
